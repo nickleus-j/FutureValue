59ca0445c78f81cabd2772477632d4f70b616d70
Nickleus Jimenez <Nickleus.Jimenez@towerswatson.com>
2022-08-25 23:42:46 +0800

    Closes #1 Preview was already implemented

--- FutureValue/FutureValue.Web/wwwroot/js/DomExtension.js
+++ FutureValue/FutureValue.Web/wwwroot/js/DomExtension.js
@@ -3,5 +3,14 @@
         let elem = document.createElement(tagName);
         elem.append(text);
         return elem;
+    },
+    ShowModal: function(Title,Text){
+        let modalElem = document.querySelector(".main-modal");
+        modalElem.classList.add("d-block");
+        modalElem.querySelector(".modal-title").innerHTML = Title;
+        modalElem.querySelector(".modal-body p").innerHTML = Text;
+        setTimeout(() => {
+            document.querySelector(".main-modal").classList.remove("d-block");
+        }, 5000)
     }
 }
