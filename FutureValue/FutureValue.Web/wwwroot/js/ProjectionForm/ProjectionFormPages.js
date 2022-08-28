5bf2714e5471318a7b5614a012c4f2dc7dd3b0f7
Nickleus Jimenez <Nickleus.Jimenez@towerswatson.com>
2022-08-29 02:51:10 +0800

    Refactored Edit and create Projection Form pages

---
+++ FutureValue/FutureValue.Web/wwwroot/js/ProjectionForm/ProjectionFormPages.js
@@ -0,0 +1,36 @@
+﻿var ProjectionFormPages = {
+    isHtmlFormReadyForApi: function (UpperBoundInterestSelector, LowerBoundInterestSelctor){
+        if (document.querySelector(".input-validation-error") || document.querySelector(".text-danger span")) {
+            DomExtension.ShowModal("Error", "Correct Validation Errors in Red");
+            return false;
+        }
+        if (parseFloat(document.querySelector(UpperBoundInterestSelector).value) < parseFloat(document.querySelector(LowerBoundInterestSelctor).value)) {
+            DomExtension.ShowModal("Error", "Upper Bound value  must never be lower than lower bound value");
+            return false;
+        }
+        return true;
+    },
+    GeneratePreviewTableBody: function (apiBody, tableSelector) {
+        var caller = new Service();
+        caller.postWithData(AppUrl + "api/Projection", apiBody, function (data) {
+            if (data == undefined || data == null) {
+                DomExtension.ShowModal("Form was not Saved", "Error ");
+            }
+            let tbl = document.querySelector(tableSelector);
+            let tBody = tbl.querySelector("tbody");
+            tbl.classList.remove("d-none");
+            tBody.innerHTML = "";
+            for (let i = 0; i < data.length; i++) {
+                let row = document.createElement("tr");
+                row.appendChild(DomExtension.MakeElement("td", "" + data[i].year));
+                row.appendChild(DomExtension.MakeElement("td", "" + data[i].startValue.toFixed(2)));
+                row.appendChild(DomExtension.MakeElement("td", "" + data[i].interestRate.toFixed(2)));
+                row.appendChild(DomExtension.MakeElement("td", "" + data[i].futureValue.toFixed(2)));
+                tBody.appendChild(row);
+            }
+
+        }, function (e) {
+            DomExtension.ShowModal("Error in contacting Server", "Try again later " + e);
+        });
+    }
+}
