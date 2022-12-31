9884b161bc8f2157cfddad798137e92cb0e8f391
Nickleus Jimenez <Nickleus.Jimenez@towerswatson.com>
2022-12-31 17:55:12 +0000

    Locale setting

--- FutureValue/FutureValue.Angular/ClientApp/src/app/futurevalues/AppSettings.ts
+++ FutureValue/FutureValue.Angular/ClientApp/src/app/futurevalues/AppSettings.ts
@@ -1,3 +1,6 @@
+import { Navigation } from "@angular/router";
+
 export class AppSettings{
-    AppUrl:string="https://localhost:7257/";
+  AppUrl: string = "https://localhost:7257/";
+  locale: string="en-US";
 }
